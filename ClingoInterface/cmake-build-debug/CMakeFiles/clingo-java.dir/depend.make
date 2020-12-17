653ab441f9d2c28a6f36f1dbfd54ac786ad917fe
StBreuer <steffen.breuer.sb@googlemail.com>
2020-12-17 12:44:33 +0100

    added defence slides

---
+++ ClingoInterface/cmake-build-debug/CMakeFiles/clingo-java.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for clingo-java.
+# This may be replaced when dependencies are built.
---
+++ ClingoInterface/cmake-build-debug/CMakeFiles/jClingo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/jClingo"
+  "CMakeFiles/jClingo.dir/java_class_filelist"
+  "CMakeFiles/jClingo.dir/java_compiled_jClingo"
+  "CMakeFiles/jClingo.dir/resources/libclingo-java.so"
+  "jClingo.jar"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/jClingo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
